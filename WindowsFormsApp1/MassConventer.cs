bccbcc820c633e4d07bbb027378c9b718796ba01
malinovicsladjana <malinovicsladjana25@gmail.com>
2026-03-27 16:30:55 +0100

    novi komentar

--- WindowsFormsApp1/MassConventer.cs
+++ WindowsFormsApp1/MassConventer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApp1
 {
-    // Ovo je komentar
+    // Ovo je komentar novi
     public class MassConventer : IConvert
     {
         public double Convert(double value)
